54113d8e26e6830999157c1b9c20d7b4878fd5dc
Cedrick Garcia <icedgarcia@gmail.com>
2025-07-23 11:14:22 +0800

    configure environment variables

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,14 +1,14 @@
+import { visionTool } from '@sanity/vision';
 import { defineConfig } from 'sanity';
 import { structureTool } from 'sanity/structure';
-import { visionTool } from '@sanity/vision';
 import schemas from './src/sanity/schemas';
 
 const config = defineConfig({
-  projectId: 'l471ihxj',
-  dataset: 'production',
-  title: 'Ced Portfolio-2025',
-  apiVersion: '2025-07-18',
-  basePath: '/studio',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+  title: process.env.NEXT_PUBLIC_SANITY_TITLE!,
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION!,
+  basePath: process.env.NEXT_PUBLIC_SANITY_BASE_PATH!,
   plugins: [visionTool(), structureTool()],
   schema: { types: schemas },
 });
--- src/sanity/config/client.ts
+++ src/sanity/config/client.ts
@@ -1,8 +1,8 @@
 import { createClient } from 'next-sanity';
 
 export const sanityClient = createClient({
-  projectId: 'l471ihxj',
-  dataset: 'production',
-  apiVersion: '2025-07-18',
-  // useCdn: process.env.NODE_ENV === 'production',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION!,
+  useCdn: process.env.NODE_ENV === 'production',
 });
